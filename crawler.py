1c98061b54f1c7331a1b92027c235e45b8010bc2
Lucas Nunes <lns2@cin.ufpe.br>
2016-06-25 03:52:03 -0300

    Add review types

--- crawler.py
+++ crawler.py
@@ -4,41 +4,54 @@ from scrapy.crawler import CrawlerProcess
 
 class ReviewSpider(scrapy.Spider):
     
-    def __init__(self, url, max_pages = 6, max_reviews = 100):
+    def __init__(self, url, user_review = True):
         self.name = "rotten crawler"
         self.allowed_domains = ["rottentomatoes.com"]
-        
-        self.url = url
+               
         self.max_pages = 6
         self.max_reviews = 100
-        
+        self.url = url + "/reviews/?page=%u"
+        self.user_review = user_review
+           
+        if (self.user_review):
+            self.url = self.url + "&type=user"
+             
         self.start_urls = [self.url % i for i in range(1, self.max_pages+1)]
         
         
     def parse(self, response):
-        with open("output.txt", 'a') as f:
-            for sel in response.xpath('//div[@class="user_review"]'):
-                review = str(sel.xpath('text()').extract())
-                f.write(review[10:len(review)-2] + "\n")
-                self.max_reviews = self.max_reviews - 1
-                
-                if (self.max_reviews <= 0):
-                    break
-   
-   
-        self.crawler.stop()
-
+        if (self.user_review):
+            skip = 10
+            element = '//div[@class="user_review"]'
+        else:
+            skip = 4
+            element = '//div[@class="the_review"]'
         
+        with open("output.txt", 'a') as f:
+            for sel in response.xpath(element):
+                review = str(sel.xpath('text()').extract())
+                
+                if (self.max_reviews > 0):
+                    f.write(review[skip:len(review)-2] + "\n")
+                else:
+                    break
+                
+                self.max_reviews = self.max_reviews - 1
+            
+                
+        self.crawler.stop()
         
         
 def get_reviews(urls):
     for url in urls:
         process = CrawlerProcess()
-        process.crawl(ReviewSpider, url)
+        process.crawl(ReviewSpider, url, False)
 
     process.start()
 
-urls = ["https://www.rottentomatoes.com/m/finding_dory/reviews/?page=%u&type=user&sort="]
+
+#Tests
+urls = ["https://www.rottentomatoes.com/m/finding_dory", "https://www.rottentomatoes.com/m/captain_america_civil_war"]
 get_reviews(urls)
 
 
