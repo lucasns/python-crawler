4d9d8fd2402a41af4888d94e66a44d34ad5b436c
Lucas Nunes <lns2@cin.ufpe.br>
2016-06-25 17:37:32 -0300

    Add ratings

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from scrapy.crawler import CrawlerProcess
 
 class ReviewSpider(scrapy.Spider):
     
-    def __init__(self, url, user_review = True, output_file = "output.txt"):
+    def __init__(self, url, user_review = True, output_file = "output"):
         self.name = "rotten crawler"
         self.allowed_domains = ["rottentomatoes.com"]
                
@@ -21,6 +21,9 @@ class ReviewSpider(scrapy.Spider):
         
         
     def parse(self, response):
+        reviews = []
+        ratings = []
+        
         if (self.user_review):
             skip = 10
             element = '//div[@class="user_review"]'
@@ -28,31 +31,61 @@ class ReviewSpider(scrapy.Spider):
             skip = 4
             element = '//div[@class="the_review"]'
         
-        with open(self.output_file, 'a') as f:
-            for sel in response.xpath(element):
-                review = str(sel.xpath('text()').extract())
-                
-                if (self.max_reviews > 0):
-                    f.write(review[skip:len(review)-2] + "\n")
-                else:
-                    break
-                
-                self.max_reviews = self.max_reviews - 1
+        #Review
+        
+        for sel in response.xpath(element):
+            review = str(sel.xpath('text()').extract())
             
+            if (self.max_reviews > 0):
+                reviews.append(review[skip:len(review)-2])
+            else:
+                break
+            
+            self.max_reviews = self.max_reviews - 1
+                     
+        #Rating
+        if (self.user_review == False):    
+            for sel in response.xpath('//div[@class="col-xs-16 review_container"]'):
+                string = str(sel.xpath("div[contains(@class, 'review_icon')]").extract())
+                string = string[15: len(string) - 10]
+                if (string == "review_icon icon small fresh"):
+                    ratings.append("fresh")
+                elif (string == "review_icon icon small rotten"):
+                    ratings.append("rotten")
                 
+        else:
+            for sel in response.xpath('//div[@class="col-xs-16"]/span[@class="fl"]'):
+                rating = 0
+                if ("xbd" in str(sel.xpath('text()').extract())):
+                    rating += 0.5
+                for s in sel.xpath('span[@class="glyphicon glyphicon-star"]'):
+                    rating += 1
+                ratings.append(rating)
+       
+                    
+        with open(self.output_file + "_reviews.txt", 'a') as f:                
+            for i in range(len(reviews)):
+                f.write(reviews[i] + "\n")
+        
+        with open(self.output_file + "_ratings.txt", 'a') as f:                
+            for i in range(len(ratings)):
+                f.write(ratings[i] + "\n")
+                
+                    
         self.crawler.stop()
         
         
 def get_reviews(urls):
     for url in urls:
         process = CrawlerProcess()
-        process.crawl(ReviewSpider, url, False, url[33:]+".txt")
+        process.crawl(ReviewSpider, url, False, url[33:])
 
     process.start()
 
 
 #Tests
-urls = ["https://www.rottentomatoes.com/m/finding_dory", "https://www.rottentomatoes.com/m/captain_america_civil_war"]
+#urls = ["https://www.rottentomatoes.com/m/finding_dory", "https://www.rottentomatoes.com/m/captain_america_civil_war"]
+urls = ["https://www.rottentomatoes.com/m/captain_america_civil_war"]
 get_reviews(urls)
 
 
